bf6a064e8d0ba0f3291ae4b246d48ddefc9ab0d0
Sebastien Viguier <viguier.sebastien@orange.fr>
2025-03-09 18:27:15 +0100

    new balance processor

--- apps/event-processor/src/processors/new-balance.processor.ts
+++ apps/event-processor/src/processors/new-balance.processor.ts
@@ -1,8 +1,11 @@
 import type { UserCreated } from "@devobank/event-bus/src/events/user-created.event";
 import { MQEventProcessor } from "@devobank/event-bus/src/processor";
+import { db } from "@devobank/read-db";
 
 export class NewBalanceProcessor extends MQEventProcessor<UserCreated> {
   async handle(event: UserCreated) {
+    const { userId } = event.payload;
+    db.insertInto("read_users").values({ id: userId, balance: 0 }).execute();
     console.log("New user created", event.payload);
   }
 }
